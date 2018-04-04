55c7e6e9c431af70e25fa2700ef3756e5bbc426e
bw99214@gmail.com <bw99214@gmailcom>
2018-04-04 17:01:15 +0530

    All Changes Commited Successfully

---
+++ JavaClasses.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaclasses;
+
+/**
+ *
+ * @author khalsa
+ */
+public class JavaClasses {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        distance object1 = new distance(2, 4); //declaring an object
+        distance object2 = object1; // refrencing an object
+    }
+    
+}
---
+++ distance.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaclasses;
+
+/**
+ *
+ * @author khalsa
+ */
+public class distance {
+     double feet = 0.0;
+    double inches = 0.0;
+    
+    distance(double feet, double inches) {
+        this.feet = feet;  // this keyword is used to point the current object
+        this.inches = inches;
+    }  //constructor of class distance
+}
